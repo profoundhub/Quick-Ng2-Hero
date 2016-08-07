08c66b199bf7a44ec603a394f72764aba3a9e17a
Daniel Lim <hbt@markhamboards.com>
2016-08-07 19:01:51 -0400

    Part 3
    Part 3

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,20 +5,11 @@ import { ROUTER_DIRECTIVES } from '@angular/router';
     directives: [ROUTER_DIRECTIVES],
     providers: [Location],
     selector: 'my-app',
-    styles: [`h1 {
-	color: white;
-	background: darkgray;
-	padding: 20px;
-}
-`],
-    template: `
-<h1>My First {{name}} app</h1>
-<router-outlet></router-outlet>
-
-<a [routerLink]="['/']">Home</a> | <a [routerLink]="['/about']">About</a>`,
+    styleUrls: ['./app.component.css'],
+    templateUrl: './app.component.html'
 })
 export class AppComponent {
-    name: string = "Angular 2 on Express";
+    pageHeader: string = "Angular 2 Hero";
 
     constructor() {}
 }
